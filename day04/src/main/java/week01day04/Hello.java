921d6b989d350b92f9608c7be4f5f1fb07fb6995
AreYouScorpio <akos.schweger@gmail.com>
2021-10-19 10:54:58 +0200

    4. feladat athelyezese GitHub-on a megfelelo helyre

---
+++ day04/src/main/java/week01day04/Hello.java
@@ -0,0 +1,9 @@
+package week01day04;
+
+public class Hello {
+    public String name;
+
+    public void writeName(String name) {
+        System.out.println(name);
+    }
+}
---
+++ day04/src/main/java/week01day04/HelloMain.java
@@ -0,0 +1,8 @@
+package week01day04;
+
+public class HelloMain {
+    public static void main(String[] args) {
+        Hello hello = new Hello ();
+        hello.writeName("Akos");
+    }
+}
